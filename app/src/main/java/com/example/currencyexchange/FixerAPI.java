062efcde473bca95358611fda870362c2a1052ac
Monia <mykhailo.bilobrovko@stud.onu.edu.ua>
2025-03-22 18:38:21 +0200

    Trird HW

--- app/src/main/java/com/example/currencyexchange/FixerAPI.java
+++ app/src/main/java/com/example/currencyexchange/FixerAPI.java
@@ -3,6 +3,7 @@ package com.example.currencyexchange; // або ваш пакет
 import retrofit2.Call;
 import retrofit2.http.GET;
 public interface FixerAPI {
+
     @GET("api/latest?access_key=2a2e0d17281101378897cb51295eceaf&symbols=USD,GBP")
     Call<String> getLatestRates();
 }
--- app/src/main/java/com/example/currencyexchange/MainActivity.java
+++ app/src/main/java/com/example/currencyexchange/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.currencyexchange; // або ваш пакет
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
